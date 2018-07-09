c9d86b2ab251bcab303af59ed6736208afc69946
Jack Luo <jackluo94@hotmail.com>
2018-07-08 17:01:11 -0700

    Init - Started Android Side
    Created Basic Android app using Xamarin

---
+++ Courses/CoursesAndroid/MainActivity.cs
@@ -0,0 +1,56 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using System;
+
+namespace CoursesAndroid
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        Button btnPrev;
+        Button btnNext;
+        TextView txtTitle, txtDescription;
+        ImageView imgCourse;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            btnNext = FindViewById<Button>(Resource.Id.btnNext);
+            btnPrev = FindViewById<Button>(Resource.Id.btnPrev);
+
+            txtTitle = FindViewById<TextView>(Resource.Id.txtTitle);
+            txtDescription = FindViewById<TextView>(Resource.Id.txtDescription);
+
+            imgCourse = FindViewById<ImageView>(Resource.Id.imgCourse);
+
+            btnNext.Click += btnNext_Click;
+            btnPrev.Click += btnPrev_Click;
+
+        }
+
+        private void btnPrev_Click(object sender, EventArgs e) {
+
+            txtTitle.Text = "Previous Clicked";
+            txtDescription.Text = "The description that appears on Previous clicked";
+            imgCourse.SetImageResource(Resource.Drawable.p1);
+
+        }
+
+        private void btnNext_Click(object sender, EventArgs e) {
+
+            txtTitle.Text = "Next Clicked";
+            txtDescription.Text = "The description that appears on Next clicked";
+            imgCourse.SetImageResource(Resource.Drawable.p2);
+
+
+        }
+    }
+}
+
