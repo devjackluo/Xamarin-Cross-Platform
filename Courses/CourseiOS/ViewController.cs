4ab7596380001bdeceaf925d2b3e75b47a68d0fe
Jack Luo <jackluo94@hotmail.com>
2018-07-12 21:20:26 -0700

    started ios

---
+++ Courses/CourseiOS/ViewController.cs
@@ -0,0 +1,50 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace CourseiOS
+{
+    public partial class ViewController : UIViewController
+    {
+		public ViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+            // Perform any additional setup after loading the view, typically from a nib.
+
+            lblTitle.Text = "Hello World";
+
+            btnPrev.TouchUpInside += BtnPrev_TouchUpInside;
+            btnNext.TouchUpInside += BtnNext_TouchUpInside;
+
+
+		}
+
+        private void BtnNext_TouchUpInside(object sender, EventArgs e) {
+
+            lblTitle.Text = "Next Clicked";
+            txtDescription.Text = "This is description for Next";
+            imgView.Image = UIImage.FromBundle("p1.jpeg");
+
+
+        }
+
+        private void BtnPrev_TouchUpInside(object sender, EventArgs e) {
+
+            lblTitle.Text = "Previous Clicked";
+            txtDescription.Text = "This is description for Previous";
+            imgView.Image = UIImage.FromBundle("p2.jpg");
+
+
+        }
+
+        public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+    }
+}
